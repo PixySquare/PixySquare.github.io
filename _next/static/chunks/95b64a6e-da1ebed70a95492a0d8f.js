3ac769d3c97feb6d9a29768718970a273f136fe6
heemankv <76938871+heemankv@users.noreply.github.com>
2021-10-16 13:13:13 +0000

    Deploying to gh-pages from  @ 33c1f4d6a3efe4ff30df3fd92ccf87d837acdc20 🚀

---
+++ _next/static/chunks/95b64a6e-da1ebed70a95492a0d8f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[866],{51649:function(t,n,u){"use strict";u.d(n,{tgG:function(){return e}});var a=u(44405);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M64 384h384v-42.666H64V384zm0-106.666h384v-42.667H64v42.667zM64 128v42.665h384V128H64z"}}]})(t)}}}]);
